13d122db8ba4fe828490e70e23cbd9b2b6415407
anfalibrahim23 <anfal.ibrahim69@gmail.com>
2015-06-23 15:50:22 +0700

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -915,7 +915,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  names = ["NotReallyABot"],
+  names = ["Wojak"],
   originalName = names[Math.floor(Math.random() * names.length)],
   sessionScore = 0,
   serverIP = "",
